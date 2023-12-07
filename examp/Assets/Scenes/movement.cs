e1e710132ff0bd252b82efa1097d5c57724c3ed0
jtmobamba <153099251+jtmobamba@users.noreply.github.com>
2023-12-07 07:37:00 +0000

    initial commit

---
+++ examp/Assets/Scenes/movement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class movement : using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed;
+    private void Start(){
+
+    }
+    private void Update(){
+        
+    }
+}
